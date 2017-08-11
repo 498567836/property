18ad0e9aee275966f8bb1aa32ee05ad560cd2b47
sun <498567836@qq.com>
2017-08-11 09:58:07 +0800

    后台在线保修管理，前台登录模板替换

---
+++ Application/Admin/Controller/PropertyController.class.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Admin\Controller;
+
+/**
+ * 后台物业管理控制器
+ * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+ */
+class PropertyController extends AdminController {
+
+    public function index(){
+        $pid = I('get.pid', 0);
+        /* 获取频道列表 */
+        $map  = array('status' => array('gt', -1), 'pid'=>$pid);
+        $list = M('Property')->where($map)->order('sort asc,id asc')->select();
+
+        $this->assign('list', $list);
+        $this->assign('pid', $pid);
+        $this->meta_title = '物业管理首页';
+        $this->display();
+    }
+    /**
+     * 文档新增页面初始化
+     * @author huajie <banhuajie@163.com>
+     */
+    public function add(){
+        //获取左边菜单
+        $this->getMenus();
+
+        $cate_id    =   I('get.cate_id',0);
+        $model_id   =   I('get.model_id',0);
+        $group_id	=	I('get.group_id','');
+
+        empty($cate_id) && $this->error('参数不能为空！');
+        empty($model_id) && $this->error('该分类未绑定模型！');
+
+        //检查该分类是否允许发布
+        $allow_publish = check_category($cate_id);
+        !$allow_publish && $this->error('该分类不允许发布内容！');
+
+        // 获取当前的模型信息
+        $model    =   get_document_model($model_id);
+
+        //处理结果
+        $info['pid']            =   $_GET['pid']?$_GET['pid']:0;
+        $info['model_id']       =   $model_id;
+        $info['category_id']    =   $cate_id;
+        $info['group_id']		=	$group_id;
+
+        if($info['pid']){
+            // 获取上级文档
+            $article            =   M('Document')->field('id,title,type')->find($info['pid']);
+            $this->assign('article',$article);
+        }
+
+        //获取表单字段排序
+        $fields = get_model_attribute($model['id']);
+        $this->assign('info',       $info);
+        $this->assign('fields',     $fields);
+        $this->assign('type_list',  get_type_bycate($cate_id));
+        $this->assign('model',      $model);
+        $this->meta_title = '新增'.$model['title'];
+        $this->display();
+    }
+
+}
